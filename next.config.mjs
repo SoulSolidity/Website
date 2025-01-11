39cdc996981416a52991c592c44c88b48e7175b7
Doublo <doublo54@gmail.com>
2025-01-11 23:14:27 +0100

    feat: new web design. shadcn-ui

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['assets.aceternity.com'], // Allow images from this domain
+    },
+};
+
+export default nextConfig;
