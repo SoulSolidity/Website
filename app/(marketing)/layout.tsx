6eab696d7ce9a9a1ddee82f214573cef771413d8
Doublo <doublo54@gmail.com>
2025-01-12 21:50:34 +0100

    remove more bullshit

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -13,10 +13,6 @@ import { SiteFooter } from "@/components/site-footer";
 
 import MobileNav from "@/components/mobile-nav";
 import { getCurrentUser } from "@/lib/session";
-import { notFound } from "next/navigation";
-import DashboardLayout from "../(dashboard)/layout";
-import { useRouter } from "next/navigation";
-import DashboardPage from "../(dashboard)/dashboard/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,54 +26,37 @@ export default async function MarketingLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const user = await getCurrentUser();
-
-  const isAuthenticated = !!user;
-
   return (
     <>
-      {isAuthenticated ? (
-        // User is authenticated, show dashboard layout
-        <>
-          <main className="flex w-full flex-col justify-center ">
-            <DashboardLayout>
-              <DashboardPage />
-            </DashboardLayout>
-          </main>
-        </>
-      ) : (
-        <>
-          <div className="flex min-h-screen flex-col">
-            <header className="h-16 container sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-              <div className="flex h-16 items-center justify-between py-6 w-full">
-                <MobileNav />
-                <MainNav />
-                <nav>
-                  <div className="md:flex">
-                    <div className="flex gap-4">
-                      <ModeToggle />
-                      <Link
-                        href="/login"
-                        className={cn(
-                          buttonVariants({ variant: "default", size: "sm" }),
-                          "px-4"
-                        )}
-                      >
-                        Contact us
-                      </Link>
-                    </div>
-                  </div>
-                </nav>
+      <div className="flex min-h-screen flex-col">
+        <header className="h-16 container sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+          <div className="flex h-16 items-center justify-between py-6 w-full">
+            <MobileNav />
+            <MainNav />
+            <nav>
+              <div className="md:flex">
+                <div className="flex gap-4">
+                  <ModeToggle />
+                  <Link
+                    href="/login"
+                    className={cn(
+                      buttonVariants({ variant: "default", size: "sm" }),
+                      "px-4"
+                    )}
+                  >
+                    Contact us
+                  </Link>
+                </div>
               </div>
-            </header>
-            {/* <HeroPage /> */}
-
-            <main className="flex-1">{children}</main>
+            </nav>
           </div>
+        </header>
+        {/* <HeroPage /> */}
 
-          <SiteFooter />
-        </>
-      )}
+        <main className="flex-1">{children}</main>
+      </div>
+
+      <SiteFooter />
     </>
   );
 }
